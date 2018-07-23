fd09297a407bb2a917e0909536dbbf8261840493
lip <543611694@qq.com>
2018-07-23 20:51:23 +0800

    add new py

--- leetcode_canPartition.py
+++ leetcode_canPartition.py
@@ -3,6 +3,14 @@
 #每一件物品的重量，每件物品的价值都等于数字的大小，背包的重量等于所有数字的和的一半
 #很无语的一点：python3超时，python2 却AC（只需要将python3代码中的range改为xrange，//改为/即可AC）
 class Solution:
+    def ZeroOnePack(self, V, weight, value):
+        F = [0] * (V + 1)
+        for i in range(0, len(weight)):
+            for v in range(V, weight[i] - 1, -1):
+                F[v] = max(F[v], F[v - weight[i]] + value[i])
+                if F[v] == V:
+                    return True
+        return False
     def canPartition(self, nums):
         """
         :type nums: List[int]
@@ -12,10 +20,4 @@ class Solution:
         if sumD%2:
             return False
         V=sumD//2
-        F=[0]*(V+1)
-        for i in range(0,len(nums)):
-            for v in range(V,nums[i]-1,-1):
-                F[v]=max(F[v],F[v-nums[i]]+nums[i])
-                if F[v]==V:
-                    return True
-        return False
+        return self.ZeroOnePack(V, nums, nums)
--- test.py
+++ test.py
@@ -1,31 +1,11 @@
-class Solution(object):
-    def helper(self, candidates, target, path, index, result):
-        if target == 0:
-            result.append(path[:])
-            return
-        prev = -1
-        for i in range(index, len(candidates)):
-            if candidates[i] > target:
-                break
-            if prev != -1 and prev == candidates[i]:
-                continue
-            self.helper(candidates, target - candidates[i], path + [candidates[i]], i + 1, result)
-            prev = candidates[i]
+def CompletePack_恰好装满方案总数( V, weight):
+    F = [0] * (V + 1)
+    F[0] = 1
+    for i in range(0, len(weight)):
+        for v in range(weight[i], V + 1):
 
-    def combinationSum2(self, candidates, target):
-        """
-        :type candidates: List[int]
-        :type target: int
-        :rtype: List[List[int]]
-        """
-        result = []
-        if not candidates:
-            return result
-
-        candidates.sort()
-        self.helper(candidates, target, [], 0, result)
-        return result
-
-
-
-print(Solution().combinationSum2( [10,1,2,7,6,1,5],8))
+                F[v] = F[v] + F[v - weight[i]]
+    return F[V]
+V = 12553
+weight = [1,2,3]
+print(CompletePack_恰好装满方案总数(V, weight))
